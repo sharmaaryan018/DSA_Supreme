0ae8dffe18148d49004dcb254016ad8d90d1c9e2
sharmaaryan18 <aryansharm3876@gmail.com>
2023-02-10 02:20:50 +0530

    10feb

---
+++ w1_assignments.cpp/solid_square_pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0;i++;i<n) {
+        for(int j=0;j++;j<n) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
