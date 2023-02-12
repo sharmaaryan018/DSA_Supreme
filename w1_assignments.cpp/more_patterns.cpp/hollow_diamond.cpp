86c9eb7d69fbfd614c7ea3eb0ee98d566e169279
sharmaaryan18 <aryansharm3876@gmail.com>
2023-02-12 23:33:13 +0530

    12 feb

--- w1_assignments.cpp/more_patterns.cpp/hollow_diamond.cpp
+++ w1_assignments.cpp/more_patterns.cpp/hollow_diamond.cpp
@@ -21,9 +21,24 @@ int main() {
        else {
         cout<<" ";
        } 
-    }
-      cout<<endl; 
+       cout<<endl;
+          }
+          for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++) {
+                cout<<" ";
+            }
 
-    } 
-    
+    for(int j=0; j<2*i-1 ;j++){
+        if(j==0||j==2*n-2*i-2) {
+            cout<<"*";
+        
+        }else {
+            cout<<" ";
+        }
+        
+       }
+      
+          }
+           cout<<endl;
+}
 }
