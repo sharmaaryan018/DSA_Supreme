54b253fe0c823556ef5d85bf2363d1d3a9b42805
sharmaaryan18 <aryansharm3876@gmail.com>
2023-03-02 01:26:22 +0530

    2 march

--- DSA.cpp/Sorting.cpp/sd.cpp
+++ DSA.cpp/Sorting.cpp/sd.cpp
@@ -3,16 +3,20 @@
 using namespace std;
   
 int main() {
-    vector<int>arr{5,4,3,2,1};
+    vector<int>arr{5,4,2,1,7,6,0};
     int n=arr.size();
     for(int i=0;i<n-1;i++) {
         int minIndex=i;
         for(int j=i+1;j<n;j++) {
             if(arr[j]<arr[minIndex]) {
-               minIndex=j; 
+                cout<<"j="<<j<<" minIndex= "<<minIndex<<endl;
+                cout<< arr[j] <<" less than " << arr[minIndex] <<endl;
+               minIndex=j;
+               cout<< j<< minIndex <<endl;
             }
         }
         swap(arr[i],arr[minIndex]);
+        cout<<"swapped "<< arr[i] << arr[minIndex]<<endl;
     }
 
     for(int i=0;i<n;i++) {
