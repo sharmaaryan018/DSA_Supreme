86ebc64afe88255fee363df02d925442a2533c51
sharmaaryan18 <aryansharm3876@gmail.com>
2023-03-16 01:08:07 +0530

    sieve_of_eratothenes

--- DSA.cpp/basic_maths.cpp/sieve_of_eratothenes.cpp
+++ DSA.cpp/basic_maths.cpp/sieve_of_eratothenes.cpp
@@ -1,4 +1,4 @@
-class Solution{
+/*class Solution{
 public:
 
     int countPrimes(int n) {
@@ -26,4 +26,41 @@ public:
         return ans;
     }
 
-};
+};*/
+
+
+//MORE OPTIMISED CODE
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<bool>Sieve(int n) {
+    vector<bool>sieve(n+1, true);
+
+    sieve[0]=sieve[1]=false;
+
+    for(int i =2;i<n;i++) {
+        if(sieve[i]==true) {
+            int j=i*i;
+
+            while(j<=n) {
+                sieve[j]=false;
+                j+=i;
+            }
+        }
+    }
+    return sieve;
+}
+
+int main() {
+    vector<bool>sieve=sieve(25);
+        for(int i=0;i<=25;i++) {
+            if(sieve[i]) {
+                cout<<i<<" ";
+            }
+        }
+    }
+
+
+
