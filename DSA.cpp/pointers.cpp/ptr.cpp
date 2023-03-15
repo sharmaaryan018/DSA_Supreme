85cdf2b5cab734c2d3bb72ba9a16eabec9871351
sharmaaryan18 <aryansharm3876@gmail.com>
2023-03-15 22:48:31 +0530

    pointer

--- DSA.cpp/pointers.cpp/ptr.cpp
+++ DSA.cpp/pointers.cpp/ptr.cpp
@@ -6,7 +6,7 @@
     }
 
 
- int msin() {
+ int main() {
 
     int a=5;
     int* p=&a;
@@ -17,10 +17,39 @@
     cout << p << endl;
     cout << *p << endl;
 
-    util(p);
+    util(p);////NO CHANGE WILL BE THERE 
     cout << "After function Calling " << endl;
     cout << a << endl;
     cout << p << endl;
     cout << *p << endl;
     return 0;
  }
+ 
+
+ /*#include<iostream>
+ using namespace std;
+
+    void util(int* p) {
+        //p=p+1;
+          *p=*p+1;
+    }
+
+
+ /*int main() {
+
+    int a=5;
+    int* p=&a;
+
+    cout<< "before "<< endl;
+    cout << a << endl;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << *p << endl;
+
+    util(p);////NO CHANGE WILL BE THERE 
+    cout << "After function Calling " << endl;
+    cout << a << endl;
+    cout << p << endl;
+    cout << *p << endl;
+    return 0;
+ }*/
