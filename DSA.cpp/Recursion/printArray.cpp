d9106d62681a98c64196717b2b083a55ff48a481
sharmaaryan18 <aryansharm3876@gmail.com>
2023-03-21 01:21:59 +0530

    print array

--- DSA.cpp/Recursion/printArray.cpp
+++ DSA.cpp/Recursion/printArray.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 void print(int arr[],int n,int i) {
@@ -16,5 +16,26 @@ int main() {
     int n=5;
     int i=0;
     print(arr,n,i);
+    return 0;
+}
+*/
+
+#include<iostream>
+using namespace std;
+
+void print(int arr[],int n) {
+    //base case
+    if(n==0) {
+        return;
+    }
+    // 1 case solved
+    cout<<arr[0]<<" ";
+    print(arr+1,n-1);
+}
+
+int main() {
+    int arr[5]={10,20,30,40,50};
+    int n=5;
+    print(arr,n);
     return 0;
 }
