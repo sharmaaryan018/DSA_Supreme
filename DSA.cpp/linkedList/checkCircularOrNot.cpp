e3f2b0dac9dc7eb37f5d0559e3a960e1a50f4828
sharmaaryan18 <aryansharm3876@gmail.com>
2023-04-24 05:46:20 +0530

    LL3

---
+++ DSA.cpp/linkedList/checkCircularOrNot.cpp
@@ -0,0 +1,155 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        this->data=0;
+        this->next=NULL;
+    }
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+
+   
+};
+
+ void print(Node* head) {
+
+        Node* temp=head;
+        while(temp!=NULL){
+            cout<< temp->data <<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    Node* getMiddle(Node* &head){
+        if(head==NULL){
+            //LL is empty
+            cout<<"LL is empty"<<endl;
+            return head;
+        }
+        //only one node in LL
+        if(head->next==NULL){
+            return head;
+        }
+        //LL having > 1 node
+
+        Node* slow = head;
+        Node* fast=head->next;
+
+        while(slow!=NULL && fast!=NULL){
+            fast = fast->next;
+
+            if(fast!=NULL){
+                fast=fast->next;
+                slow=slow->next;
+            }
+        }
+        return slow;
+    }
+
+   /* int getLength(Node* head){
+        int len =0;
+        Node* temp=head;
+        while(temp!=NULL){
+            temp=temp->next;
+            len++;
+        }
+        return len;
+    }*/
+
+  /* Node* reverseKNodes(Node* head,int k)
+{
+    if(head==NULL){
+        cout<<"LL is Empty"<<endl;
+        return NULL;
+    }
+    int len=getLength(head);
+    if(k>len){
+        return head;
+    }
+
+    //it means o. of nodes in ll is >=k
+    //step->A: revrese first k nodes of LL
+    Node* prev=NULL;
+    Node* curr=head;
+    Node* forward=curr->next;
+
+    int  count =0;
+
+    while(count<k){
+        forward=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=forward;
+        count++;
+    }
+    if(forward!=NULL){
+        //we still have nodes to reverse
+       head->next= reverseKNodes(forward,k);
+    }
+    //step C: return head of the modified LL
+    return prev;
+}*/
+
+    bool checkForLoop(Node* &head){
+        if(head == NULL ){
+            cout<<"LL is Empty"<<endl;
+            return false;
+        }
+
+        Node* slow=head;
+        Node* fast=head;
+
+        while(fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+             fast=fast->next;
+             slow=slow->next;
+            }
+        }
+
+        if(slow==fast){
+            //loop present
+            return true;
+        }
+        //fast null hogya
+        return false;
+    }
+int main() { 
+    Node* head= new Node(10);
+    Node* second= new Node(20);
+    Node* third= new Node(30);
+    Node* fourth= new Node(40);
+    Node* fifth= new Node(50);
+    Node* sixth= new Node(60);
+    Node* seven= new Node(70);
+    Node* eight= new Node(80);
+    Node* nine= new Node(90);
+
+    head->next=second;
+    second->next=third;
+    third->next=fourth;
+    fourth->next=fifth;
+    fifth->next=sixth;
+    sixth->next=seven;
+    seven->next=eight;
+    eight->next=nine;
+    nine->next=fifth;
+
+    
+    print(head);
+   // head=reverseKNodes(head,3);
+    //print(head);
+   // cout<< "Middle node is: "<<getMiddle(head)->data<<endl;
+
+   cout<<"Loop is present or not: "<<checkForLoop(head)<<endl;
+
+    return 0;
+}
