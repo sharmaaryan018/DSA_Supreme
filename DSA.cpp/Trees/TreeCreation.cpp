89beb43d31a915506822c6b219c986c63a053bc7
sharmaaryan18 <aryansharm3876@gmail.com>
2023-05-11 05:22:35 +0530

    TreeCreation_Trees_lec1

---
+++ DSA.cpp/Trees/TreeCreation.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+Node* buildTree() {
+    int data;
+    cout<<"Enter the data: "<<endl;
+    cin>>data;
+
+    if(data==-1) {
+        return NULL;
+    }
+    //STEPS-> A, B and C.
+
+    Node* root= new Node(data);
+
+    cout<<"Enter the data for left node : "<<data<<endl;
+    root->left=buildTree();
+
+    
+    cout<<"Enter the data for right node : "<<data<<endl;
+    root->right=buildTree();
+
+    return root;
+}
+
+int main() {
+
+    Node* root = NULL;
+
+    root= buildTree();
+    return 0;
+}
