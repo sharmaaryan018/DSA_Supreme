284c02ea4b7ce6525707a6a50ce60ee869dea94f
sharmaaryan18 <aryansharm3876@gmail.com>
2023-08-20 19:31:07 +0530

    20 august

--- DSA.cpp/linkedList/linkedlist.cpp
+++ DSA.cpp/linkedList/linkedlist.cpp
@@ -10,12 +10,11 @@ class Node {
         this->data=0;
         this->next=NULL;
     }
-
+  
     Node(int data){
         this->data=data;
         this->next=NULL;
     }
-
    
 };
 
@@ -29,19 +28,50 @@ class Node {
     }
 
 int main() { 
-    Node* first= new Node(10);
-    Node* second= new Node(20);
-    Node* third= new Node(30);
-    Node* fourth= new Node(40);
-    Node* fifth= new Node(50);
+    // Node* first= new Node(10);
 
-    first->next=second;
-    second->next=third;
-    third->next=fourth;
-    fourth->next=fifth;
+    // Node* second= new Node(20);
+    // Node* third= new Node(30);
+    // Node* fourth= new Node(40);
+    // Node* fifth= new Node(50);
+    Node* head = new Node();
+    
+    while(true)
+    {
+        int ch;
+        cout << "nter your choice\n";
+        cin >> ch;
+
+        if(ch==0) break;
+        else {
+            int val;
+            cout << "Enter your value\n";
+            cin >> val;
+            
+            Node* new_node = new Node(val);
+            if(head->data==0  && head->next==NULL)
+              {
+                 head->data = new_node->data;
+                 continue;
+              }
+            Node* temp = head;
+
+            while(temp->next!=NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next  = new_node;
+        }
+    }
+
+    // first->next=second;
+    // second->next=third;
+    // third->next=fourth;
+    // fourth->next=fifth;
 
     cout<< "printing the Linked list"<<endl;
-    print(first);
+    print(head);
 
     return 0;
 }
---
+++ DSA.cpp/linkedList/llpractice.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val)
+    {
+        data  = val;
+        next = NULL;
+    }
+};
+
+void print(node* temp)
+{
+    // node* temp = head;
+    while(temp)
+    {
+        cout << temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+void reversell(node* head, int k)
+{
+    node* temp = head;
+    vector<int> v;
+    int c = 0;
+    while(k>=c)
+    {
+
+        v.push_back(temp->data);
+        temp = temp->next;
+        c++;
+    }
+
+    reverse(v.begin(),v.end());
+    for(int i=0;i<=k;i++)
+    {
+        cout << v[i];
+    }
+
+    int c1=0;
+    temp  = head;
+    while(c1<c)
+    {
+        temp = temp->next;
+        c1++;
+    }
+
+    while(temp)
+    {
+        cout << temp->data;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    node* reverse_node = new node(10);
+     node* second = new node(20);
+      node* third = new node(30);
+       node* fourth = new node(40);
+        node* fifth = new node(50);
+
+        reverse_node->next = second;
+        second->next = third;
+        third->next = fourth;
+        fourth->next = fifth;
+
+        // print(reverse_node);
+        reversell(reverse_node,2);
+}
