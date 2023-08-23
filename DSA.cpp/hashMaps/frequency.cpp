4e798fb5dc4ee93b0da3b09abd3f7fa944dd2363
sharmaaryan18 <aryansharm3876@gmail.com>
2023-08-23 23:31:49 +0530

    frequencyCount

--- DSA.cpp/hashMaps/frequency.cpp
+++ DSA.cpp/hashMaps/frequency.cpp
@@ -2,6 +2,25 @@
 #include<unordered_map>
 using namespace std;
 
+bool checkCircularnode(Node* head) {
+    unordered_map<Node*, bool> vis;
+    Node* temp = head;
+
+
+    while(temp!=NULL) {
+        if(vis.find(temp)!=vis.end()) {
+             vis[temp]=true;
+        }
+        else {
+            return true;
+        }
+        temp= temp->next;
+
+    }
+    return false;
+
+}
+
 int main() {
 
     string str = "thiruvananthanpuram";
