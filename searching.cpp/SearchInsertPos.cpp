90cc0858e4f42e91cffa4630dd7ec2c6c51becd8
sharmaaryan18 <aryansharm3876@gmail.com>
2024-02-08 23:57:49 +0530

    Bs

---
+++ searching.cpp/SearchInsertPos.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size(); // size of the array
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (nums[mid] >= target) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+};
