eb305077e5d4d1ded99b87c85b2fa9dda3df1cde
sharmaaryan18 <aryansharm3876@gmail.com>
2024-02-15 00:00:51 +0530

    commit

--- searching.cpp/first&lastOcc.cpp
+++ searching.cpp/first&lastOcc.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-         vector<int> ans(2,-1);
-    if(nums.size()==0)return ans;
+         vector<int> ans1(2,-1);
+    if(nums.size()==0)return ans1;
     int t1=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     int t2=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-    if(t1==t2)return ans;
+    if(t1==t2)return ans1;
     cout<<t1<<t2;
     ans[0]=t1;
     ans[1]=t2-1;
-    return ans;
+    return ans1;
     }
 };
